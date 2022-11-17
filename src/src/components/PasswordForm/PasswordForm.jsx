617b49b34ecbb8cd53643115eb8ea2ef976d8832
kekkei9 <kekkei2002@gmail.com>
2022-11-17 16:12:52 +0700

    update PasswordForm

---
+++ src/src/components/PasswordForm/PasswordForm.jsx
@@ -0,0 +1,22 @@
+import { Input } from 'antd';
+import React from 'react'
+import './PasswordForm.scss'
+
+
+const PasswordForm = ({ title, placeholder }) => {
+  return <div className="PasswordForm">
+    <div className='tw-text-base tw-ml-3 tw-mb-1.5'>{title}</div>
+    <Input.Password
+      placeholder={placeholder}
+      style = {{
+        borderRadius: '10px',
+        padding: '7px 12px',
+        border: 'solid #D9D9D9',
+        display: 'block',
+        width: '400px'
+      }}
+    />
+  </div>
+}
+
+export default PasswordForm
