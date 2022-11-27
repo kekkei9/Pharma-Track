281badb5ab625c5a2b3c4b2f452966c0a7d75e83
PandoraLH <lixhen2002@gmail.com>
2022-11-27 19:37:26 +0700

    add color to step

--- src/src/components/BookApBox/BookApBox.jsx
+++ src/src/components/BookApBox/BookApBox.jsx
@@ -3,21 +3,33 @@ import './BookApBox.scss'
 import BookApTab1 from '../BookApTab1/BookApTab1'
 import BookApTab2 from '../BookApTab2/BookApTab2';
 import BookApTab3 from '../BookApTab3/BookApTab3';
-import { Button } from 'antd';
 import StepComponent from '../StepComponent/StepComponent';
+
+import { Button } from 'antd';
 
 
 const BookApBox = (props) => {
 
   const [current, setCurrent] = useState(1);
 
-  return <div className = "Box">
-    <div className = "Boxes tw-flex tw-mx-auto tw-max-w-5xl tw-mt-12 ">
-      <StepComponent step = 'Bước 1' content = 'Chọn phòng khám' ></StepComponent>
+  return <div className = "Box"> 
+  
+    {current == 1 ? <div className = "Boxes tw-flex tw-mx-auto tw-max-w-5xl tw-mt-12 ">
+      <StepComponent step = 'Bước 1' content = 'Chọn phòng khám' style ={{background: '#20A0D8', color: 'white'}} ></StepComponent>
       <StepComponent step = 'Bước 2' content = 'Phiếu thông tin' ></StepComponent>
       <StepComponent step = 'Bước 3' content = 'Xác nhận & Thanh Toán' ></StepComponent>
-    </div>
-    
+    </div> : current == 2 ? 
+    <div className = "Boxes tw-flex tw-mx-auto tw-max-w-5xl tw-mt-12 ">
+    <StepComponent step = 'Bước 1' content = 'Chọn phòng khám' ></StepComponent>
+    <StepComponent step = 'Bước 2' content = 'Phiếu thông tin' style ={{background: '#20A0D8', color: 'white'}} ></StepComponent>
+    <StepComponent step = 'Bước 3' content = 'Xác nhận & Thanh Toán' ></StepComponent>
+    </div> : 
+    <div className = "Boxes tw-flex tw-mx-auto tw-max-w-5xl tw-mt-12 ">
+    <StepComponent step = 'Bước 1' content = 'Chọn phòng khám' ></StepComponent>
+    <StepComponent step = 'Bước 2' content = 'Phiếu thông tin' ></StepComponent>
+    <StepComponent step = 'Bước 3' content = 'Xác nhận & Thanh Toán' style ={{background: '#20A0D8', color: 'white'}} ></StepComponent>
+    </div> }
+
     {current == 1 ? <BookApTab1 /> : current == 2 ? <BookApTab2 /> : <BookApTab3/>}
     <div className = 'tw-flex tw-justify-center tw-my-12'>
       {current >1 && (
