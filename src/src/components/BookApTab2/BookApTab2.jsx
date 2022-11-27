ea4c9785a1edf4bc00db30297e49052630a66f94
PandoraLH <lixhen2002@gmail.com>
2022-11-27 14:20:42 +0700

    test

--- src/src/components/BookApTab2/BookApTab2.jsx
+++ src/src/components/BookApTab2/BookApTab2.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import InputForm from '../InputForm/InputForm'
 import './BookApTab2.scss'
 
 
 const BookApTab2 = (props) => {
-  return <div className="BookApTab2">BookApTab2</div>
+  return <div className="Container">
+    <div>
+      1.Thông tin người đăng ký
+    </div>
+    <div>
+      <InputForm title = 'Họ và tên (*)' placeholder = 'Họ và tên' className = 'tw-max-w-xl'>
+        </InputForm> 
+    </div>
+  </div>
 }
 
 export default BookApTab2
