865387bf0de4d523c071ccc90478d8f0600d5acc
kekkei9 <kekkei2002@gmail.com>
2022-11-27 11:36:03 +0700

    style as props

--- src/src/components/InputForm/InputForm.jsx
+++ src/src/components/InputForm/InputForm.jsx
@@ -3,18 +3,18 @@ import { Input } from 'antd'
 import './InputForm.scss'
 
 
-const InputForm = ({ title, placeholder }) => {
+const InputForm = ({ title, placeholder , style}) => {
   return <div className="InputForm">
     <div className='tw-text-base tw-ml-3'>{title}</div>
     <Input 
       placeholder={placeholder} 
-      style = {{
+      style = {Object.assign({
         borderRadius: '10px',
         padding: '7px 12px',
         border: 'solid #D9D9D9',
         width: '400px',
-        marginTop: '6px'
-      }}
+        marginTop: '6px',
+      }, style)}
     />
   </div>
 }
--- src/src/components/PasswordForm/PasswordForm.jsx
+++ src/src/components/PasswordForm/PasswordForm.jsx
@@ -3,18 +3,18 @@ import React from 'react'
 import './PasswordForm.scss'
 
 
-const PasswordForm = ({ title, placeholder }) => {
+const PasswordForm = ({ title, placeholder, style }) => {
   return <div className="PasswordForm">
     <div className='tw-text-base tw-ml-3 tw-mb-1.5'>{title}</div>
     <Input.Password
       placeholder={placeholder}
-      style = {{
+      style = {Object.assign({
         borderRadius: '10px',
         padding: '7px 12px',
         border: 'solid #D9D9D9',
         display: 'inline-flex',
         width: '400px'
-      }}
+      }, style)}
     />
   </div>
 }
