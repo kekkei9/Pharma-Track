9d4e09def697949f5d720e5ffbdf6098601466f9
kekkei9 <kekkei2002@gmail.com>
2022-12-09 17:39:34 +0700

    footer fit width & remove condition

--- src/src/components/Footer/Footer.jsx
+++ src/src/components/Footer/Footer.jsx
@@ -1,24 +1,21 @@
-import React from 'react'
-import './Footer.scss'
-import { useLocation, useNavigate } from 'react-router-dom'
+import React from "react";
+import "./Footer.scss";
 
 const Footer = (props) => {
-    const location = useLocation()
-    const navigate = useNavigate()
-
-  return (<div className="Footer tw-mt-6">
-    <div className='tw-pl-20 tw-pt-4 tw-pb-4 tw-pr-20 tw-flex tw-flex-col tw-items-center' style={{backgroundColor: '#0067A9'}}>
-      {location.pathname === '/home' || location.pathname === '/' ? <div className = 'RegBtn tw-px-12 tw-py-2.5 tw-bg-white tw-font-semibold tw-text-xl'
-        onClick={() => navigate('/bookap')}>
-        Đặt khám ngay!
-      </div> : <></>}
-      <ul className='tw-self-start'>
-        {['Copyright', 'About', 'Contact'].map((prop) => (
-          <li className='tw-text-white '>{prop}</li>
-        ))}
-      </ul>
+  return (
+    <div className="Footer tw-mt-6">
+      <div
+        className="tw-pl-20 tw-pt-4 tw-pb-4 tw-pr-20 tw-flex tw-flex-col tw-items-center"
+        style={{ backgroundColor: "#0052d4" }}
+      >
+        <ul className="tw-self-start">
+          {["Copyright", "About", "Contact"].map((prop) => (
+            <li className="tw-text-white ">{prop}</li>
+          ))}
+        </ul>
+      </div>
     </div>
-  </div>)
-}
+  );
+};
 
-export default Footer
+export default Footer;
