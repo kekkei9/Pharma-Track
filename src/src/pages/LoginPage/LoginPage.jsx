5303801fa30c49abf46bca4173709305675c2a14
kekkei9 <kekkei2002@gmail.com>
2022-12-09 17:45:40 +0700

    fix the page

--- src/src/pages/LoginPage/LoginPage.jsx
+++ src/src/pages/LoginPage/LoginPage.jsx
@@ -2,12 +2,25 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import './LoginPage.scss'
 import LoginForm from '../../components/LoginForm/LoginForm'
+import TestFormContainer from '../../containers/TestForm/TestForm.container'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 
 const LoginPage = (props) => {
   const navigate = useNavigate()
 
-  return <div className="LoginPage">
+  return <div className="LoginPage tw-flex tw-flex-col tw-items-center">
+    <div style={{ width: '400px'}}>
+      <div className='back-button tw-flex tw-flex-row tw-items-center'
+        onClick={() => navigate(-1)}>
+        <FontAwesomeIcon icon={faArrowLeft}/>
+        <div className='tw-ml-2 tw-text-base'>
+          Trở lại
+        </div>
+      </div>
+    <TestFormContainer/>
     <LoginForm/>
+    </div>
   </div>
 }
 
--- src/src/pages/SignUpPage/SignUpPage.jsx
+++ src/src/pages/SignUpPage/SignUpPage.jsx
@@ -2,11 +2,22 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import SignUpForm from '../../components/SignUpForm/SignUpForm'
 import './SignUpPage.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 
 const SignUpPage = (props) => {
   const navigate = useNavigate()
 
-  return <div className="SignUpPage">
+  return <div className="SignUpPage tw-flex tw-flex-col tw-items-center">
+    <div style={{ width: '400px'}}>
+      <div className='back-button tw-flex tw-flex-row tw-items-center'
+        onClick={() => navigate(-1)}>
+        <FontAwesomeIcon icon={faArrowLeft}/>
+        <div className='tw-ml-2 tw-text-base'>
+          Trở lại
+        </div>
+      </div>
+    </div>
     <SignUpForm />
   </div>
 }
