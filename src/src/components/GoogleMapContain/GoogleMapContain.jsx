bb23b386d1b0252f60e37565d3a22d80d399d0b6
PandoraLH <lixhen2002@gmail.com>
2022-12-15 22:12:29 +0700

    update button set location

--- src/src/components/GoogleMapContain/GoogleMapContain.jsx
+++ src/src/components/GoogleMapContain/GoogleMapContain.jsx
@@ -1,12 +1,23 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import './GoogleMapContain.scss'
-import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
-import GoogleMapInstance from '../GoogleMapInstance/GoogleMapInstance';
+import { GoogleMap, useLoadScript, MarkerF } from '@react-google-maps/api';
 import GetAddress from '../GetAddress/GetAddress';
 import DoctorCard from '../DoctorCard/DoctorCard';
-import { Button } from 'antd';
 import BackNextButton from '../BackNextButton/BackNextButton';
+
+
+const libraries = ["places"]
+
+const containerStyle = {
+  width: '600px',
+  height: '600px'
+};
+
+const options = {
+  disableDefaultUI: true,
+  zoomControl: true,
+};
 
 const DoctorData = [
   {
@@ -38,19 +49,15 @@ const DoctorData = [
   },
 ]
 
-const containerStyle = {
-  width: '600px',
-  height: '600px'
-};
-
-
 
 const GoogleMapContain = () => {
 
   const [doctorState, setDoctorState] = useState(0)
 
   const navigate = useNavigate()
-  
+
+  const location = GetAddress();
+
   const handleDoubleClick = (props) => {
     navigate('/bookap/doctor' + props.id)
   }
@@ -59,25 +66,54 @@ const GoogleMapContain = () => {
     navigate('/homepage')
   }
 
+
   const onClickNext = () => {
     navigate('/bookap/doctor' + doctorState)
   }
 
-  const location = GetAddress()
-
-  const { isLoaded } = useLoadScript({
-    googleMapsApiKey: 'AIzaSyCXyl8cV0kbV42iLv6qmXmSU5wZie9F2n4'
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: 'AIzaSyCXyl8cV0kbV42iLv6qmXmSU5wZie9F2n4',
+    libraries,
   });
-  
+
+  const mapRef = React.useRef();
+
+  const onMapLoad = React.useCallback((map) => {
+    mapRef.current = map;
+  }, []);
+
+  const panTo = React.useCallback(({ lat, lng }) => {
+    mapRef.current.panTo({ lat, lng });
+    mapRef.current.setZoom(17);
+  }, []);
+
+  if (loadError) return <div>Error...</div>;
+
   if (!isLoaded) return <div>Loading...</div>
 
   return <div className='GoogleMapContain'>
     <div className = 'container tw-flex tw-justify-center tw-items-center'>
-      <GoogleMapInstance
-        mapContainerStyle = { containerStyle }
-        center = { location.coordinates }
-        zoom = {15} 
-        setDoctorState = {setDoctorState}/>
+      <div className = 'GoogleMap'>
+        <Locate panTo={panTo}/> 
+        <GoogleMap
+          id="map"
+          mapContainerStyle={ containerStyle }
+          zoom={17}
+          center={location.coordinates}
+          options={options}
+          onLoad={onMapLoad}
+      >
+        <MarkerF position = { location.coordinates }/>
+          {DoctorData.map((doctor) => (<MarkerF 
+                                    position = {{ lat : doctor.lat , lng : doctor.lng}}
+                                    options = {{
+                                      icon : '/assets/cliniclogo.png'}}
+                                    onClick = {() => {
+                                      setDoctorState(doctor.id)
+                                      panTo({lat: doctor.lat, lng: doctor.lng})}}
+                                    />))}
+      </GoogleMap>
+      </div>
       <div className = 'tw-ml-20'>
         <DoctorCard {...DoctorData[doctorState]} 
                       handleDoubleClick = {() => handleDoubleClick(DoctorData[doctorState])}/> 
@@ -87,6 +123,20 @@ const GoogleMapContain = () => {
     <BackNextButton onClickBack={ onClickBack } onClickNext = { onClickNext }/>
   
   </div>
+}
+
+function Locate({ panTo }) {
+  const location = GetAddress();
+  return (
+    <button
+      className="locate"
+      onClick={() => {
+        panTo(location.coordinates)
+      }}
+    >
+      <img src="/assets/currentlocation.png" alt="location" width='40px' height = '40px'/>
+    </button>
+  );
 }
 
 export default GoogleMapContain
